1eb2019dd151153233db2e406e224633ee688771
Satherov <satherov@users.noreply.github.com>
2025-08-20 12:20:50 +0200

    align with the drill recipe adjustments in base IF

--- src/main/java/com/buuz135/industrialforegoingsouls/data/IFSoulsSerializableRecipe.java
+++ src/main/java/com/buuz135/industrialforegoingsouls/data/IFSoulsSerializableRecipe.java
@@ -1,5 +1,7 @@
 package com.buuz135.industrialforegoingsouls.data;
 
+import net.neoforged.neoforge.common.crafting.SizedIngredient;
+
 import com.buuz135.industrial.recipe.LaserDrillOreRecipe;
 import com.buuz135.industrial.recipe.LaserDrillRarity;
 import com.buuz135.industrialforegoingsouls.IndustrialForegoingSouls;
@@ -20,8 +22,7 @@ public class IFSoulsSerializableRecipe {
     public static void init(RecipeOutput output) {
         var biomeRarity = new LaserDrillRarity.BiomeRarity(List.of(SoulTags.Biomes.DEEP_DARK), List.of());
         var dimensionRarity = new LaserDrillRarity.DimensionRarity(List.of(), List.of());
-        var laser = new LaserDrillOreRecipe(Ingredient.of(Items.ECHO_SHARD), 11,
-                new LaserDrillRarity(biomeRarity, dimensionRarity, -64, 20, 1));
+        var laser = new LaserDrillOreRecipe(SizedIngredient.of(Items.ECHO_SHARD, 1), 11, new LaserDrillRarity(biomeRarity, dimensionRarity, -64, 20, 1));
         ResourceLocation rl = ResourceLocation.fromNamespaceAndPath(IndustrialForegoingSouls.MOD_ID, "laser_drill_ore/echo_shard");
         AdvancementHolder advancementHolder = output.advancement().addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(rl)).rewards(AdvancementRewards.Builder.recipe(rl)).requirements(AdvancementRequirements.Strategy.OR).build(rl);
         output.accept(rl, laser, advancementHolder);
